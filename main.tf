f42806a0f5965b088aaae494718c91fde9575a6a
Steven Tisdol <98587233+stisdol612@users.noreply.github.com>
2022-05-14 23:56:06 -0400

    Completed Terraform File

--- main.tf
+++ main.tf
@@ -64,6 +64,139 @@ resource "aws_subnet" "database-subnet-2" {
   }
 }
 
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "LU-IGW"
+  }
+}
+
+# Create Web layber route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "WebRT"
+  }
+}
+
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.web-subnet-1.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.web-subnet-2.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+#Create EC2 Instance
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0022f774911c1d690"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id              = aws_subnet.web-subnet-1.id
+  user_data              = file("install_apache.sh")
+  tags = {
+    Name = "Web Server"
+  }
+
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-0022f774911c1d690"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1b"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id              = aws_subnet.web-subnet-2.id
+  user_data              = file("install_apache.sh")
+  tags = {
+    Name = "Web Server"
+  }
+
+}
+
+# Create Web Security Group
+resource "aws_security_group" "web-sg" {
+  name        = "Web-SG"
+  description = "Allow inbound traffic HTTP"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description = "HTTP into VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-SG"
+  }
+}
+
+resource "aws_lb" "external-elb" {
+  name               = "External-LB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web-sg.id]
+  subnets            = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
+}
+
+resource "aws_lb_target_group" "external-elb" {
+  name     = "ALB-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my-vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "external-elb1" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.webserver1,
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "external-elb2" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.webserver2,
+  ]
+}
+
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-elb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-elb.arn
+  }
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_subnet_group_name = aws_db_subnet_group.default.id
