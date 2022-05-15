9b6f15ab7f5cf2be5d8e0aa48d4aa804b71894ef
Steven Tisdol <98587233+stisdol612@users.noreply.github.com>
2022-05-15 01:41:58 -0400

    Sudo Commit

--- install_apache.sh
+++ install_apache.sh
@@ -4,4 +4,6 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
+sudo firewall-cmd — permanent — add-service=http
+sudo firewall-cmd — reload
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
