6401de87f9f907ab26aeee5baa524138072ead1e
Steven Tisdol <98587233+stisdol612@users.noreply.github.com>
2022-05-15 00:03:45 -0400

    Trying GitHub

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_route_table_association" "a" {
 
 resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.web-subnet-2.id
-  route_table_id = aws_route_table.web-rt.id
+  route_table_id = aws_route_table.web-rt.i
 }
 
 #Create EC2 Instance
