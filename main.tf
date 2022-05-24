4721eabe496f6200c6c9f0b4a469104fae624efe
stisdol612 <steven.tisdol@gmail.com>
2022-05-23 20:47:12 -0400

    First Commit

--- main.tf
+++ main.tf
@@ -211,7 +211,7 @@ resource "aws_db_instance" "default" {
 }
 
 resource "aws_db_subnet_group" "default" {
-  name       = "lu"
+  name       = "luit"
   subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
 
   tags = {
