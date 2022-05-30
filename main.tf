64ec7521bf9827eed46869c13ef84a2b30f46271
stisdol612 <steven.tisdol@gmail.com>
2022-05-30 01:03:47 -0400

    First commit

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 # Create a VPC
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
